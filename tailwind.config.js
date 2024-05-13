c931336b2d46632ca2ff5b0a82a218280d957e2c
Fahad Ghouri <mrlegendg007@gmail.com>
2024-05-13 06:30:29 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./scrap.html",
+    // Or if you're using JS frameworks:
+    // "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
